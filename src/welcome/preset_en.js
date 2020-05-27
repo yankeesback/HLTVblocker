1c850e4716553f3ac07b68be06dcb9543f9711de
yankeesback <58868692+yankeesback@users.noreply.github.com>
2020-05-27 14:52:16 +0000

    Add files via upload

---
+++ src/welcome/preset_en.js
@@ -0,0 +1,15 @@
+var PRESET_RULES = [{
+        name: "HLTV",
+        url: "https://www.hltv.org/",
+        rules: [{
+                "title": "HLTV BLOCKER",
+                "search_block_by_css": true,
+                "hide_block_by_css": true,
+                "site_regexp": "www.hltv.org",
+                "specify_url_by_regexp": false,
+                "search_block_css": "DIV.standard-box",
+                "hide_block_css": "DIV.standard-box"
+            }]
+    }, 
+   ];
+//# sourceMappingURL=preset_en.js.map
